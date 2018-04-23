1d5f2d80b2b2c50fcf8550fc340fb2451a404ec4
uxff <uxff@qq.com>
2018-04-23 23:02:51 +0800

    update requester

--- example/http/casprequester.go
+++ example/http/casprequester.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/http"
 
 	"github.com/uxff/webservice-client-as-service-provider/casp"
 )
@@ -14,15 +15,26 @@ func main() {
 	flag.StringVar(&caspServerAddr, "server", caspServerAddr, "casp server addr")
 	flag.StringVar(&caspServerName, "name", caspServerName, "casp server name")
 
-	allNodes := casp.GetNodes()
+	allNodes := "" //casp.GetNodes()
 
 	if len(allNodes) == 0 {
 		fmt.Printf("the no nodes can serve you\n")
-		return
+		//return
 	}
 
 	if len(allNodes) > 0 {
-		casp.Request(allNodes[0], &casp.SimpleRequest{Method: "GET", Uri: "http://www.soso.com/", Header: []string{"None: none"}})
+		//casp.Request(allNodes[0], &casp.SimpleRequest{Method: "GET", Uri: "http://www.soso.com/", Header: []string{"None: none"}})
+	}
+
+	req := casp.SimpleRequest{Method: "GET", Uri: "http://www.soso.com/", Header: []string{"None: none"}}
+	reqHttp, err := http.NewRequest(req.Method, req.Uri, nil)
+	if err != nil {
+		//
+	}
+	cli := &http.Client{}
+	resHttp, err := cli.Do(reqHttp)
+	if err != nil {
+
 	}
 
 	return
